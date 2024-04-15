58bfe620b27896a0091d4610716f196ab1db6d70
Amro Abdalla <amroadel@aucegypt.edu>
2024-04-15 00:38:59 -0400

    Update bnc_spoken.py

--- preprocess/bnc_spoken.py
+++ preprocess/bnc_spoken.py
@@ -27,7 +27,11 @@ def preprocess(f):
         prev_line = line
 
 
-with open("../data/babylm_data/babylm_100M/bnc_spoken.train") as f:
-    with open("../data/processed/bnc_spoken.txt", 'w') as g:
-        for line in preprocess(f):
-            g.write(f"{line}\n")
+def process_data(dataset):
+    input_path = f"../data/babylm_data/babylm_100M/{dataset}/bnc_spoken.{dataset}"
+    output_path = f"../data/processed/{dataset}/bnc_spoken_{dataset}.txt"
+
+    with open(input_path) as f:
+        with open(output_path, 'w') as g:
+            for line in preprocess(f):
+                g.write(f"{line}\n")
